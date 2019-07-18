cf92f387ccb83ae88d2c29b33b56132aa9dbfc76
ZJL2019 <379105244@qq.com>
2019-07-18 09:46:21 +0800

    顺序表 增删改查

---
+++ 0717/0717/main.c
@@ -0,0 +1,35 @@
+#include "seqlist.h"
+
+int capacity = 10;
+SeqList test;
+int main()
+{
+	SeqListInit(&test, capacity);
+	SeqListPushBack(&test, 1);
+	SeqListPushBack(&test, 2);
+	SeqListPushBack(&test, 3);
+	SeqListPushBack(&test, 4);
+	SeqListPushBack(&test, 5);
+	SeqListPushBack(&test, 6);
+	SeqListPushBack(&test, 7);
+	SeqListPushBack(&test, 8);
+	SeqListPushFront(&test, 8);
+	SeqListPushFront(&test, 9);
+	SeqListPushFront(&test, 10);
+	SeqListPopBack(&test);
+	SeqListPopFront(&test);
+	SeqListInsert(&test, 2, 5);
+	SeqListErase(&test, 2);
+	SeqListRemove(&test, 5);
+	SeqListModify(&test, 1, 9);
+	SeqListPrint(&test);
+
+	//SeqListRemoveAll(&test, 9);
+	SeqListRemoveAllN(&test, 9);
+	SeqListBubbleSort(&test);
+	SeqListPrint(&test);
+	printf("%d\n", SeqListDiscFind(&test, 2, 0, 5));
+	printf("%d\n",SeqListFind(&test, 1));
+	system("pause");
+	return 0;
+}
---
+++ 0717/0717/seqlist.c
@@ -0,0 +1,187 @@
+#include "seqlist.h"
+
+
+void SeqListInit(SeqList* psl, size_t capacity)
+{
+	psl->array = (SeqList*)calloc(capacity, sizeof(SLDataType));
+	psl->capacity = capacity;
+	psl->size = 0;
+}
+void SeqListDestory(SeqList* psl)
+{
+	if (!psl->capacity)
+	{
+		free(psl->array);
+		psl->capacity = NULL;
+		psl->size = 0;
+	}
+}
+void CheckCapacity(SeqList* psl)
+{
+	if (psl->size >= psl->capacity)
+	{
+		psl->capacity *= 2;
+	}
+	psl->array = (SeqList*)realloc(psl->array, psl->capacity*sizeof(SLDataType));
+}
+void SeqListPushBack(SeqList* psl, SLDataType x)
+{
+	CheckCapacity(psl);
+	psl->array[psl->size] = x;
+	psl->size++;
+}
+void SeqListPopBack(SeqList* psl)
+{
+	psl->size--;
+}
+void SeqListPushFront(SeqList* psl, SLDataType x)
+{
+	CheckCapacity(psl);
+	int i;
+	for (i = psl->size-1; i>=0; i--)
+	{
+		psl->array[i + 1] = psl->array[i];
+	}
+	psl->array[0] = x;
+	psl->size++;
+}
+void SeqListPopFront(SeqList* psl)
+{
+	int i;
+	for (i = 0; i<psl->size; i++)
+	{
+		psl->array[i] = psl->array[i + 1];
+	}
+	psl->size--;
+}
+int SeqListFind(SeqList* psl, SLDataType x)
+{
+	int i;
+	for (i = 0; i < psl->size; i++)
+	{
+		if (psl->array[i] == x)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+void SeqListInsert(SeqList* psl, size_t pos, SLDataType x)
+{
+	CheckCapacity(psl);
+	int i;
+	for (i = psl->size - 1; i < pos; i--)
+	{
+		psl->array[i + 1] = psl->array[i];
+	}
+	psl->array[pos] = x;
+	psl->size++;
+}
+void SeqListErase(SeqList* psl, size_t pos)
+{
+	int i;
+	for (i =pos; i<psl->size; i++)
+	{
+		psl->array[i] = psl->array[i + 1];
+	}
+	psl->size--;
+}
+void SeqListRemove(SeqList* psl, SLDataType x)
+{
+	int i;
+	for (i = SeqListFind(psl, x); i<psl->size; i++)
+	{
+		psl->array[i] = psl->array[i + 1];
+	}
+	psl->size--;
+}
+void SeqListRemoveAll(SeqList* psl, SLDataType x)//bug
+{
+	int i;
+	int gap = 0;
+	for (i = 0; i < psl->size-gap; i++)
+	{
+		if (psl->array[i + gap] == x)
+		{
+			gap++;
+			if (i + gap >= psl->capacity)
+			{
+				break;
+			}
+		}
+		psl->array[i] = psl->array[i + gap];
+	}
+	psl->size -= gap;
+}
+
+void SeqListRemoveAllN(SeqList* psl, SLDataType x)
+{
+	int capacity = psl->capacity;
+	SLDataType*arry1 = (SLDataType*)calloc(capacity, sizeof(SLDataType));
+	int i, j;
+	for (i = 0, j = 0; i <psl->size; i++)
+	{
+		if (psl->array[i] != x)
+		{
+			arry1[j] = psl->array[i];
+			j++;
+		}
+	}
+	free(psl->array);
+	psl->array = arry1;
+	psl->size = j ;
+}
+
+void SeqListBubbleSort(SeqList* psl)
+{
+	int i, j;
+	int tmp = 0;
+	for (i = 0; i < psl->size-1; i++)
+	{
+		for (j = 0; j < psl->size - i-1; j++)
+		{
+			if (psl->array[j]>psl->array[j + 1])
+			{
+				tmp = psl->array[j];
+				psl->array[j] = psl->array[j + 1];
+				psl->array[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+int SeqListDiscFind(SeqList* psl, SLDataType x,SLDataType left,SLDataType right)
+{
+	int mid = 0;
+	while (left<=right)
+	{
+		int mid = left + (right + left) / 2;
+		if (psl->array[mid] < x)
+		{
+			left = mid + 1;
+		}
+		else if (psl->array[mid]>x)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;
+}
+
+void SeqListModify(SeqList* psl, size_t pos, SLDataType x)
+{
+	psl->array[pos] = x;
+}
+void SeqListPrint(SeqList* psl)
+{
+	int i = 0;
+	for (i = 0; i < psl->size; i++)
+	{
+		printf("%d ", psl->array[i]);
+	}
+	putchar('\n');
+}
