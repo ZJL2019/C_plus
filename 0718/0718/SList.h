87438ef267395f207b0a7b4e0137989492ae4c1f
ZJL2019 <379105244@qq.com>
2019-07-19 09:00:53 +0800

    链表基础

---
+++ 0718/0718/SList.h
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#ifndef _SLIST_C_
+#define _SLIST_C_
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef int SLTDataType;
+
+typedef struct SListNode{
+	SLTDataType data;
+	struct SListNode*next;
+}SListNode;
+
+
+void SListInit(SListNode** pphead);
+void SListDestory(SListNode* phead);
+SListNode* BuySListNode(SLTDataType x);
+void SListPushFront(SListNode** pphead, SLTDataType x);
+void SListPopFront(SListNode** pphead);
+SListNode* SListFind(SListNode* phead, SLTDataType x);
+void SListRemove(SListNode** pphead, SLTDataType x); 
+void SListRemoveAll(SListNode** pphead, SLTDataType x);
+void SListInsertAfter(SListNode* pos, SLTDataType x);
+void SListEraseAfter(SListNode* pos);
+void SListReverse(SListNode** pphead);
+void SListReverse2(SListNode** pphead);
+void SListPrint(SListNode* phead);
+#endif /*_SLIST_C_*/
---
+++ 0718/0718/main.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "SList.h"
+
+
+int main()
+{
+	SListNode *head;
+	SListInit(&head);
+
+	SListPushFront(&head, 9);
+	SListPushFront(&head, 8);
+	SListPushFront(&head, 7);
+	SListPushFront(&head, 6);
+	SListPushFront(&head, 5);
+	SListPushFront(&head, 4);
+	SListPushFront(&head, 3);
+	SListPushFront(&head, 2);
+	SListRemoveAll(&head, 1);
+	//SListReverse(&head);
+	//SListReverse2(&head);
+	SListInsertAfter(SListFind(head,2), 1);
+	/*SListEraseAfter(SListFind(head, 5));*/
+	/*SListPopFront(&head);
+	SListPopFront(&head);*/
+	//SListDestory(&head);
+	SListPrint(head);
+	system("pause");
+	return 0;
+}
+
+int __main()
+{
+	SListNode*phead;
+	SListNode*plast;
+	SListNode*cur;
+	int m = 6, n = 5;
+	SListInit(&phead);
+	SListPushFront(&phead, m);
+	plast = phead;
+	int i;
+	for (i = m - 1; i>=1; i--)
+	{
+		SListPushFront(&phead, i);
+	}
+	plast->next = phead;
+
+	cur = plast;
+	for (m = 6; m > 1; m--)
+	{
+		for (i = 1; i < n; i++)
+		{
+			cur = cur->next;
+		}
+		SListEraseAfter(cur);
+	}
+	printf("%d\n", cur->data);
+	free(cur);
+	system("pause");
+	return 0;
+
+}
