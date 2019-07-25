ca8f4fda86d5c1a415a1c7d35393fd270ef5a99d
ZJL2019 <379105244@qq.com>
2019-07-25 18:10:08 +0800

    前序遍历二叉树 中序遍历二叉树 后序遍历二叉树 层序遍历二叉树

--- 0724/0724/BTree.c
+++ 0724/0724/BTree.c
@@ -1,5 +1,5 @@
 #include"BTree.h"
-
+#include"queue.h"
 BTNode* BinaryTreeCreate(BTDataType* a)
 {
 	BTNode*cur;
@@ -16,4 +16,62 @@ BTNode* BinaryTreeCreate(BTDataType* a)
 	cur->right = BinaryTreeCreate(a);
 
 	return cur;
+}
+
+
+void BinaryTreePrevOrder(BTNode* root)
+{
+	if (root != NULL)
+	{
+		printf("%c", root->data);
+		BinaryTreePrevOrder(root->left);
+		BinaryTreePrevOrder(root->right);
+	}
+	
+
+}
+
+void BinaryTreeInOrder(BTNode* root)
+{
+	if (root != NULL)
+	{
+		BinaryTreeInOrder(root->left);
+		printf("%c", root->data);
+		BinaryTreeInOrder(root->right);
+	}
+}
+
+void BinaryTreePostOrder(BTNode* root)
+{
+	if (root != NULL)
+	{
+		BinaryTreePostOrder(root->left);
+		BinaryTreePostOrder(root->right);
+		printf("%c", root->data);
+	}
+}
+
+void BinaryTreeLevelOrder(BTNode* root)
+{
+	Queue qu;
+	BTNode*tmp;
+
+	QueueInit(&qu);
+	QueuePush(&qu, root);
+
+	while (QueueIsEmpty(&qu))
+	{
+		tmp = QueueFront(&qu);
+		printf("%c ", tmp->data);
+		if (tmp->left)
+		{
+			QueuePush(&qu, tmp->left);
+		}
+		if (tmp->right)
+		{
+			QueuePush(&qu, tmp->right);
+		}
+		QueuePop(&qu);
+	}
+
 }
