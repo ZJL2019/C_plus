9329b9c04507075753d153d01115c4ea62cd89f9
ZJL2019 <379105244@qq.com>
2019-09-06 13:10:22 +0800

    更新注释，以及测试数据

--- class_data/test_1/test_1/Sqlist.h
+++ class_data/test_1/test_1/Sqlist.h
@@ -15,12 +15,12 @@ typedef struct SeqList{
 	SLDataType length;
 }SeqList;
 
-void InitList(SeqList *psl);
-void ShowList(SeqList *psl);
-SLDataType ListEmpty(SeqList psl);
-SLDataType GetELem(SeqList psl, SLDataType i);
-SLDataType LocateElem(SeqList psl, SLDataType e);
-void ListInsert(SeqList *psl, SLDataType i, SLDataType e);
-void ListDelete(SeqList *psl, SLDataType i);
-void MergeList(SeqList *psl, SeqList *psl2);
+void InitList(SeqList *psl);//初始化线性表
+void ShowList(SeqList *psl);//输出线性表
+SLDataType ListEmpty(SeqList psl);//判断线性表是否为空
+SLDataType GetELem(SeqList psl, SLDataType i);//获取线性表中的第i个元素
+SLDataType LocateElem(SeqList psl, SLDataType e);//查找数值为e的元素
+void ListInsert(SeqList *psl, SLDataType i, SLDataType e);//在第i个元素后插入数值为e的元素
+void ListDelete(SeqList *psl, SLDataType i);//删除线性表中的第i个元素
+void MergeList(SeqList *psl, SeqList *psl2);//合并两个有序线性表为降序序列
 #endif /*_SQLIST_C_*/
--- class_data/test_1/test_1/main.c
+++ class_data/test_1/test_1/main.c
@@ -4,7 +4,7 @@ SeqList test;
 SeqList test2;
 int main()
 {
-	/*InitList(&test);
+	InitList(&test);
 	ListInsert(&test, 1, 3);
 	ListInsert(&test, 2, 6);
 	ListInsert(&test, 3, 9);
@@ -17,19 +17,21 @@ int main()
 	printf("%d\n",GetELem(test, 2));
 	printf("%d\n", LocateElem(test, 9));
 	ListDelete(&test, 3);
-	ShowList(&test);*/
+	ShowList(&test);
+
+
 	InitList(&test);
-	ListInsert(&test, 1, 10);
-	ListInsert(&test, 2, 8);
-	ListInsert(&test, 3, 6);
-	ListInsert(&test, 4, 4);
-	ListInsert(&test, 5, 2);
+	ListInsert(&test, 1, 29);
+	ListInsert(&test, 2, 18);
+	ListInsert(&test, 3, 12);
+	ListInsert(&test, 4, 7);
+	ListInsert(&test, 5, 6);
 	InitList(&test2);
-	ListInsert(&test2, 1, 9);
-	ListInsert(&test2, 2, 7);
-	ListInsert(&test2, 3, 5);
-	ListInsert(&test2, 4, 3);
-	ListInsert(&test2, 5, 1);
+	ListInsert(&test2, 1, 33);
+	ListInsert(&test2, 2, 22);
+	ListInsert(&test2, 3, 17);
+	ListInsert(&test2, 4, 12);
+	ListInsert(&test2, 5, 5);
 	MergeList(&test, &test2);
 	ShowList(&test);
 	return 0;
