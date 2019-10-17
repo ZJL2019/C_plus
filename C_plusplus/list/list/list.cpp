94ba872ee5ceb2b06a24aa500e5cd40edd9d8de0
ZJL2019 <379105244@qq.com>
2019-10-17 16:44:16 +0800

    list模拟实现

---
+++ C_plusplus/list/list/list.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+namespace bite
+{
+	//list：带头结点的双向循环链表
+	template<class T>
+	struct ListNode
+	{
+		ListNode(const T& data = T())
+		:_pNext(nullptr)
+		, _pPre(nullptr)
+		, _data(data)
+		{}
+
+		ListNode<T>* _pNext;
+		ListNode<T>* _pPre;
+		T _data;
+	};
+
+	//list迭代器：将节点类型的指针重新封装
+	template<class T>
+	struct list_iterator
+	{
+		typedef ListNode<T> Node;
+		typedef list_iterator<T> Self;
+	public:
+		list_iterator(Node* pCur)
+			:_pCur(pCur)
+		{}
+		//按照指针的方式进行应用
+		T& operator*()
+		{
+			return _pCur->_data;
+		}
+
+		T* operator->()
+		{
+			return &(_pCur->_data);
+		}
+
+		//移动
+		Self& operator++()
+		{
+			_pCur = _pCur->_pNext;
+			return *this;
+		}
+
+		Self operator++(int)
+		{
+			Self temp(*this);
+			_pCur = _pCur->_pNext;
+			return temp;
+		}
+
+		Self& operator--()
+		{
+			_pCur = _pCur->_pPre;
+		}
+		Self operator--(int);
+
+			Node* _pCur;
+	};
+}
