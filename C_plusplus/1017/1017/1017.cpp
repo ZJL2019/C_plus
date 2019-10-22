cf2a172c6d8e55c7c91b70cfc02ce057faaf17fd
ZJL2019 <379105244@qq.com>
2019-10-22 13:18:50 +0800

    更新

--- C_plusplus/1017/1017/1017.cpp
+++ C_plusplus/1017/1017/1017.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+
+
 #include<queue>
 #include<vector>
 #include<functional>
@@ -36,3 +38,63 @@ int main()
 	cout << q2.top() << endl;
 	return 0;
 }
+
+
+#if 0
+class Date
+{
+	friend class Compare;
+public:
+	Date(int year, int month, int day)
+		:_year(year)
+		, _month(month)
+		, _day(day)
+
+	{}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+class Compare
+{
+public:
+	bool operator()(Date* pLeft, Date* pRight)
+	{
+		if (pLeft->_day < pRight->_day)
+		{
+			return true;
+		}
+		return false;
+	}
+};
+
+#include<queue>
+
+int main()
+{
+	Date d1(2019, 10, 18);
+	Date d2(2019, 10, 16);
+	Date d3(2019, 10, 17);
+	
+#if 0
+	//比较方式：默认的less
+	priority_queue<Date> q1;
+
+	//如果优先级队列中插入自定义类型的元素，
+	//在插入元素期间，优先级队列必须进行元素的比较（less: <  greater: > )
+	//相当于需要对存储的自定义类型元素进行大于或者小于的比较
+	//类中： 必须重载> 或者 <的符号
+	q1.push(d1);
+	q1.push(d2);
+	q1.push(d3);
+#endif
+	priority_queue<Date*, vector<Date*>, Compare> q2;
+	q2.push(&d1);
+	q2.push(&d2);
+	q2.push(&d3);
+	return 0;
+}
+#endif
