29212b0d64325ecea03f0bbb9d4e03a341786115
ZJL2019 <379105244@qq.com>
2019-11-09 23:16:08 +0800

    习题1,2

---
+++ C_plusplus/习题/习题/test1.cpp
@@ -0,0 +1,51 @@
+//【组队竞赛】牛牛举办了一次编程比赛, 参加比赛的有3*n个选手, 每个选手都有一个水平值a_i.现在要将这些选手进行组队, 一共组成n个队伍, 即每个队伍3人.牛牛发现
+//队伍的水平值等于该队伍队员中第二高水平值。
+//例如 :
+//一个队伍三个队员的水平值分别是3, 3, 3.那么队伍的水平值是3
+//一个队伍三个队员的水平值分别是3, 2, 3.那么队伍的水平值是3
+//一个队伍三个队员的水平值分别是1, 5, 2.那么队伍的水平值是2
+//为了让比赛更有看点, 牛牛想安排队伍使所有队伍的水平值总和最大。
+//如样例所示 :
+//如果牛牛把6个队员划分到两个队伍
+//如果方案为 :
+//team1 : {1, 2, 5}, team2 : {5, 5, 8}, 这时候水平值总和为7.
+//而如果方案为 :
+//   team1 : {2, 5, 8}, team2 : {1, 5, 5}, 这时候水平值总和为10.
+//		   没有比总和为10更大的方案, 所以输出10.
+
+
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+
+long RetMaxSum(vector<long>& v,int n)
+{
+	long ret=0;
+	sort(v.begin(), v.end());
+	while (n>0)
+	{
+		v.pop_back();
+		ret += v[v.size()-1];
+		v.pop_back();
+		n--;
+	}
+	return ret;
+}
+
+int main()
+{
+	int temp;
+	int n;
+	cin >> n;
+	int i = 3 * n;
+	vector<long> v;
+	while (i > 0)
+	{
+		cin >> temp;
+		v.push_back(temp);
+		i--;
+	}
+	cout << RetMaxSum(v, n) << endl;
+	return 0;
+}
---
+++ C_plusplus/习题/习题/test2.cpp
@@ -0,0 +1,33 @@
+//【删除公共字符】输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。例如，输入”They are students.”和”aeiou”，则删除之后的第一个字符串变
+//成”Thy r stdnts.”
+
+#include<iostream>
+using namespace std;
+#include<string>
+
+void RemoveStr(string& s1, string& s2)
+{
+	char str[255] = { 0 };
+	for (int i = 0; i < s2.length(); i++)
+	{
+		str[s2[i]]++;
+	}
+	for (int i = 0; i < s1.length(); i++)
+	{
+		if (str[s1[i]]==0)
+		{
+			cout << s1[i];
+		}
+	}
+	cout << endl;
+}
+
+int main()
+{
+	string str1, str2;
+	while (getline(cin, str1) && getline(cin, str2))
+	{
+		RemoveStr(str1, str2);
+	}
+	return 0;
+}
