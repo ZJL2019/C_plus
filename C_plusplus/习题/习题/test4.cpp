38fec1b52089edad57c4738009b75ace987346a8
ZJL2019 <379105244@qq.com>
2019-11-11 19:25:43 +0800

    day2_2

---
+++ C_plusplus/习题/习题/test4.cpp
@@ -0,0 +1,25 @@
+//将一句话的单词进行倒置，标点不倒置。比如 I like beijing.经过函数后变为：beijing.like I
+
+
+#include<iostream>
+using namespace std;
+#include<string>
+
+int main()
+{
+	string str;
+	while (getline(cin, str))
+	{
+		for (int i = str.length() - 1; i >= 0; i--)
+		{
+			if (str[i] == ' ')
+			{
+				string str_r = str.substr(i + 1);
+				cout << str_r << " ";
+				str.resize(i);
+			}
+		}
+		cout << str;
+	}
+	return 0;
+}
