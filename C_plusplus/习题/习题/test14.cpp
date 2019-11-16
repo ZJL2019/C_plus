9d1d606efb3d0d154d0c3d0def910fcc8c6bf0bc
ZJL2019 <379105244@qq.com>
2019-11-16 17:08:26 +0800

    day7_2

---
+++ C_plusplus/习题/习题/test14.cpp
@@ -0,0 +1,39 @@
+//36939 - 合法括号序列判断
+//对于一个字符串，请设计一个算法，判断其是否为一个合法的括号串。
+//
+//给定一个字符串A和它的长度n，请返回一个bool值代表它是否为一个合法的括号串。
+#include<iostream>
+using namespace std;
+#include<string>
+#include<stack>
+class Parenthesis {
+public:
+	bool chkParenthesis(string A, int n) {
+		stack<char> sc;
+		for (auto ele : A)
+		{
+			switch (ele)
+			{
+			case '(':
+				sc.push(ele);
+				break;
+			case ')':
+			{
+				if (sc.empty() || sc.top() != '(')
+				{
+					return false;
+				}
+
+				else
+				{
+					sc.pop();
+				}
+			}
+				break;
+			default:
+				return false;
+			}
+		}
+		return true;
+	}
+};
