3a03b462810ab758a83bf0ddd3337ec5aa901ff6
ZJL2019 <379105244@qq.com>
2019-11-18 22:31:04 +0800

    day9_1

---
+++ C_plusplus/习题/习题/test17.cpp
@@ -0,0 +1,22 @@
+//请编写一个函数，将两个数字相加。不得使用 + 或其他算数运算符。
+//
+//给定两个int A和B。请返回A＋B的值
+
+#include<iostream>
+using namespace std;
+
+class UnusualAdd {
+public:
+	int addAB(int A, int B) {
+		int sum = 0, carry = 0;
+		while (B != 0)
+		{
+			sum = A^B;
+			carry = (A&B) <<1;
+			A = sum;
+			B = carry;
+		}
+		return sum;
+
+	}
+};
