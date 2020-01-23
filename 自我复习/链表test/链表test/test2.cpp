3dfe7f48c4cebf3fc2ba6fc8e0ae3ba0374f56ce
ZJL2019 <379105244@qq.com>
2020-01-23 19:27:37 +0800

    数据结构复习:---------->add 双链表合并

--- 自我复习/链表test/链表test/test2.cpp
+++ 自我复习/链表test/链表test/test2.cpp
@@ -3,15 +3,24 @@
 int main()
 {
 	List test;
+	List test2;
 	ListInit(&test);
-	ListPushBack(&test, 1);
+	ListInit(&test2);
+
 	ListPushBack(&test, 1);
 	ListPushBack(&test, 3);
-	ListPushBack(&test, 4);
 	ListPushBack(&test, 5);
-	ListPushBack(&test, 6);
+	ListPushBack(&test, 7);
+	ListPushBack(&test, 9);
+	ListPushBack(&test2, 2);
+	ListPushBack(&test2, 4);
+	ListPushBack(&test2, 6);
+	ListPushBack(&test2, 8);
+	ListPushBack(&test2, 10);
+
 	//ListPopBack(&test);
-	ListRemove(&test, 1);
+	//ListRemove(&test, 1);
+	ListMerge(&test, &test2);
 	ListPrint(&test);
 	return 0;
 }
